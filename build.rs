dd198f08cd694075de01783428a00e776773e1e7
Javier Viola <javier@parity.io>
2025-07-07 18:02:24 +0200

    more work

---
+++ build.rs
@@ -0,0 +1,6 @@
+fn main() {
+	substrate_build_script_utils::generate_cargo_keys();
+	// For the node/worker version check, make sure we always rebuild the node and binary workers
+	// when the version changes.
+	substrate_build_script_utils::rerun_if_git_head_changed();
+}
